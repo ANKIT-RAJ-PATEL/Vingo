ab56247ccd2026a77b86674dc3e9230ed14a54b4
ANKIT-RAJ-PATEL <ankitraj61370@gmail.com>
2026-04-28 22:50:35 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,8 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"https://vingo-fullstack-e60w.onrender.com",
     credentials:true
 }))
 
